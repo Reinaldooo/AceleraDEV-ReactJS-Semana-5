dfdeca169f758c25c31491a52a56ea866bd1c808
Reinaldo Trindade <rewifetri@gmail.com>
2020-05-04 17:02:44 -0300

    get api data on App.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { Component } from "react";
 
-class Contact extends React.Component {
+class Contact extends Component {
+  formatDate(date) {
+    return new Date(date).toLocaleDateString();
+  }
+
   render() {
-    return (<div />);
+    const {
+      avatar,
+      name,
+      company,
+      department,
+      admissionDate,
+      phone,
+      country,
+    } = this.props.data;
+
+    return (
+      <article className="contact" data-testid="contact">
+        <span className="contact__avatar" data-testid="contact-avatar">
+          {avatar && <img src={avatar} alt={name} />}
+        </span>
+        <span className="contact__data" data-testid="contact-name">{name}</span>
+        <span className="contact__data" data-testid="contact-phone">{phone}</span>
+        <span className="contact__data" data-testid="contact-country">{country}</span>
+        <span className="contact__data" data-testid="contact-date">{this.formatDate(admissionDate)}</span>
+        <span className="contact__data" data-testid="contact-company">{company}</span>
+        <span className="contact__data" data-testid="contact-department">{department}</span>
+      </article>
+    );
   }
 }
 
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { ReactComponent as LogoSvg } from "../assets/img/logo.svg";
 
-class Topbar extends React.Component {
-	render() {
-		return (<div />);
-	}
+class TopBar extends React.Component {
+  render() {
+    return (
+      <header data-testid="topbar" className="topbar">
+        <div className="container">
+          <a href="/" className="topbar__logo">
+            <LogoSvg alt="Logo" />
+          </a>
+        </div>
+      </header>
+    );
+  }
 }
 
-export default Topbar;
+export default TopBar;
